bc65ada90f3d6f698f25db818f5c0349391490e1
Selahattin <selahattinbal06@gmail.com>
2022-04-20 21:56:53 +0300

    refactor

--- src/app/services/productfilters.service.ts
+++ src/app/services/productfilters.service.ts
@@ -22,26 +22,24 @@ sendData(data:string){
 //Filter methods for products 
 starFilterService(star:any){
   if(star.target.checked){
-    this.filteringURL+=`&star_gte=${star.target.value}`
-    this.api.filterApi(this.filteringURL).subscribe(res=>this.dataSource.next(res))
+    this.filteringURL+=`&star_gte=${star.target.value}` 
   }
   if(!(star.target.checked)){
     let replace=`&star_gte=${star.target.value}`
     this.filteringURL= this.filteringURL.replace(replace,"")
-    this.api.filterApi(this.filteringURL).subscribe(res=>this.dataSource.next(res))
   }
+  this.api.filterApi(this.filteringURL).subscribe(res=>this.dataSource.next(res))
  }
 
 brandFilterService(brand:any){
   if(brand.target.checked){
     this.filteringURL+=`&brand=${brand.target.value}`
-    this.api.filterApi(this.filteringURL).subscribe(res=>this.dataSource.next(res))
   }
   if(!(brand.target.checked)){
     let replace=`&brand=${brand.target.value}`
     this.filteringURL= this.filteringURL.replace(replace,"")
-    this.api.filterApi(this.filteringURL).subscribe(res=>this.dataSource.next(res))
   }
+  this.api.filterApi(this.filteringURL).subscribe(res=>this.dataSource.next(res))
 }
 
 priceFilterService(min:any,max:any){
@@ -50,13 +48,9 @@ priceFilterService(min:any,max:any){
     {let replace=`&price_gte=${min}`
     this.filteringURL= this.filteringURL.replace(/&price_gte=\d+/,replace)}
    else{this.filteringURL+=`&price_gte=${min}`} 
-    this.api.filterApi(this.filteringURL).subscribe(res=>this.dataSource.next(res))
   }
-  if(min===""){
-    this.filteringURL.replace(`&price_gte=${min}`,"")
-    let replace=`&price_gte=${min}`
-    this.filteringURL= this.filteringURL.replace(replace,"")
-    this.api.filterApi(this.filteringURL).subscribe(res=>this.dataSource.next(res))
+  if(min==""){
+    this.filteringURL= this.filteringURL.replace(/&price_gte=\d+/,"")
   }
 
   if(!(max==="")){
@@ -64,14 +58,11 @@ priceFilterService(min:any,max:any){
     { let replace=`&price_lte=${max}`
     this.filteringURL= this.filteringURL.replace(/&price_lte=\d+/,replace)}
    else{this.filteringURL+=`&price_lte=${max}`} 
-   this.api.filterApi(this.filteringURL).subscribe(res=>this.dataSource.next(res))
   }
-  if(max===""){
-    this.filteringURL.replace(`&price_lte=${max}`,"")
-    let replace=`&price_lte=${max}`
-    this.filteringURL= this.filteringURL.replace(replace,"")
-    this.api.filterApi(this.filteringURL).subscribe(res=>this.dataSource.next(res))
+  if(max==""){
+    this.filteringURL= this.filteringURL.replace(/&price_lte=\d+/,"")
   }
+  this.api.filterApi(this.filteringURL).subscribe(res=>this.dataSource.next(res))
 }
 
 categoryFilterService(category:string){
@@ -80,12 +71,11 @@ categoryFilterService(category:string){
     {let replace=`&category=${category}`
     this.filteringURL= this.filteringURL.replace(/&category=[a-z]+/,replace)}
     else{this.filteringURL+=`&category=${category}`}
-    this.api.filterApi(this.filteringURL).subscribe(res=>this.dataSource.next(res))
   }
   if(category==''){
     this.filteringURL= this.filteringURL.replace(/&category=[a-z]+/,"")
-    this.api.filterApi(this.filteringURL).subscribe(res=>this.dataSource.next(res))
   }
+  this.api.filterApi(this.filteringURL).subscribe(res=>this.dataSource.next(res))
 }
 
 }
