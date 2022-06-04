da38bd19f381025cc39edff270e378053a91d86b
Selahattin <selahattinbal06@gmail.com>
2022-06-04 23:19:07 +0300

    rating in comment section added

--- src/app/component/products/products.component.ts
+++ src/app/component/products/products.component.ts
@@ -8,48 +8,54 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
-
-export class ProductsComponent implements OnInit,OnDestroy {
-  private products: any
+export class ProductsComponent implements OnInit, OnDestroy {
+  private products: any;
   //For unsubcribing of subscriptions in ngDestroy taking subscriptions to a variable
-  private sub1$:any
-  private sub2$:any
+  private sub1$: any;
+  private sub2$: any;
   //var. for templates
   public star: number[] = [1, 2, 3, 4, 5];
-  public filteredProducts: any
+  public filteredProducts: any;
   //Case (For using async pipe which automatically unsub. when component destroy)
-  public searchKey$?: Observable<string>
-  public viewGrid:boolean = true
-  public uniqueBrands:any = []
+  public searchKey$?: Observable<string>;
+  public viewGrid: boolean = true;
+  public uniqueBrands: any = [];
 
-  constructor(private api: ApiService, private cartService: CartService, private router: Router, private toastr: ToastrService,private productFilters:ProductfiltersService) { }
+  constructor(
+    private api: ApiService,
+    private cartService: CartService,
+    private router: Router,
+    private toastr: ToastrService,
+    private productFilters: ProductfiltersService
+  ) {}
 
   ngOnInit(): void {
     //getting product from server
-    this.sub1$=this.api.getAllProductApi()
-      .subscribe(res => {
-        this.products = res;
-        this.filteredProducts = res;
-        //checking unique brands for same brand inputs from db 
-        //so can display dynamically in brand filter
-        this.products.forEach((element: any) => {
-          return this.uniqueBrands.push(element.brand)
-        })
-        this.uniqueBrands = this.uniqueBrands.filter((item: any,
-          index: any) => this.uniqueBrands.indexOf(item) === index);
+    this.sub1$ = this.api.getAllProductApi().subscribe((res) => {
+      this.products = res;
+      this.filteredProducts = res;
+      //checking unique brands for same brand inputs from db
+      //so can display dynamically in brand filter
+      this.products.forEach((element: any) => {
+        return this.uniqueBrands.push(element.brand);
       });
+      this.uniqueBrands = this.uniqueBrands.filter(
+        (item: any, index: any) => this.uniqueBrands.indexOf(item) === index
+      );
+    });
     // data transfer for search handling ( filter in subcribe not working so pipe is used)
     // async pipe usage case
-    this.searchKey$=this.cartService.search
-    this.sub2$= this.productFilters.data.subscribe(res=>this.filteredProducts=res)
+    this.searchKey$ = this.cartService.search;
+    this.sub2$ = this.productFilters.data.subscribe(
+      (res) => (this.filteredProducts = res)
+    );
   }
-
 
   // Sending element id to product detail paramMap
   onSelect(item: any) {
-    this.router.navigate(["/products", item.id])
+    this.router.navigate(['/products', item.id]);
   }
   // adding item to cart
   addtocart(item: any) {
@@ -59,36 +65,34 @@ export class ProductsComponent implements OnInit,OnDestroy {
 
   //changing view style
   viewHandler(category: string) {
-    if (category === "grid") {
-      this.viewGrid = true
+    if (category === 'grid') {
+      this.viewGrid = true;
     }
-    if (category === "list") {
-      this.viewGrid = false
+    if (category === 'list') {
+      this.viewGrid = false;
     }
   }
 
-  
   //Filtering operation events(handling by productfilter.service)
   categoryFilter(category: string) {
-    this.productFilters.categoryFilterService(category)
+    this.productFilters.categoryFilterService(category);
   }
 
   priceFilter(min: any, max: any) {
-    this.productFilters.priceFilterService(min,max)
+    this.productFilters.priceFilterService(min, max);
   }
 
-  brandFilter(brand: any) { 
-    this.productFilters.brandFilterService(brand)
+  brandFilter(brand: any) {
+    this.productFilters.brandFilterService(brand);
   }
 
   starFilter(star: any) {
-  this.productFilters.starFilterService(star)
+    this.productFilters.starFilterService(star);
   }
 
   //Unsubscribing to the subscriptions
   ngOnDestroy(): void {
-      this.sub1$?.unsubscribe()
-      this.sub2$?.unsubscribe()
+    this.sub1$?.unsubscribe();
+    this.sub2$?.unsubscribe();
   }
-
 }
--- src/app/component/productsdetail/productsdetail.component.ts
+++ src/app/component/productsdetail/productsdetail.component.ts
@@ -8,39 +8,40 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-productsdetail',
   templateUrl: './productsdetail.component.html',
-  styleUrls: ['./productsdetail.component.css']
+  styleUrls: ['./productsdetail.component.css'],
 })
 export class ProductsdetailComponent implements OnInit, OnDestroy {
-  private sub$:any
+  private sub$: any;
   //for star template
   public star: number[] = [1, 2, 3, 4, 5];
   //public bc they will reached by template
-  public productsDetail: any
-  public currentUrl!: string
-  public commentForm!: FormGroup
-  public rating=3
-  private itemId:any
-  constructor(private route: ActivatedRoute, private api: ApiService, private cartService: CartService, private toastr: ToastrService, private formBuilder: FormBuilder,private http:HttpClient) { }
+  public productsDetail: any;
+  public currentUrl!: string;
+  public commentForm!: FormGroup;
+  public rating = 3;
+  private itemId: any;
+  constructor(
+    private route: ActivatedRoute,
+    private api: ApiService,
+    private cartService: CartService,
+    private toastr: ToastrService,
+    private formBuilder: FormBuilder,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
-    //With paramMap you can route multiple page with only one template 
-    this.itemId=Number(this.route.snapshot.paramMap.get("id"))
-    this.currentUrl =`?id=${this.itemId}`
-    //For template view taking data from server 
-   this.sub$=this.api.filterApi(this.currentUrl).subscribe(
-      res => {
-        this.productsDetail = res;
-      })
+    //With paramMap you can route multiple page with only one template
+    this.itemId = Number(this.route.snapshot.paramMap.get('id'));
+    this.currentUrl = `?id=${this.itemId}`;
+    //For template view taking data from server
+    this.sub$ = this.api.filterApi(this.currentUrl).subscribe((res) => {
+      this.productsDetail = res;
+    });
 
-      this.commentForm = this.formBuilder.group({
-        username:["",Validators.required],
-        comment:["",Validators.required]
-      })
-
-
-
-
-
+    this.commentForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      comment: ['', Validators.required],
+    });
   }
   //adding to cart from product detail page
   addtocart(item: any) {
@@ -48,22 +49,28 @@ export class ProductsdetailComponent implements OnInit, OnDestroy {
     this.toastr.success('Item Added To Cart', 'Checkout');
   }
 
-  submit(){
-    let date=new Date().toString()
-    let comment={"username":this.commentForm.value.username,"comment":this.commentForm.value.comment}
-   Object.assign(comment,{"time":date})
-    // STAR service yazılacak EKLENECEK 
-    this.productsDetail[0].comments.push(comment)
-    this.api.updateItemApi(this.productsDetail[0],this.itemId).subscribe(res => {
-    this.commentForm.reset()
-    this.toastr.success('Product Added', 'Success');
-  })
+  submit() {
+    let date = new Date().toString();
+    let comment = {
+      username: this.commentForm.value.username,
+      comment: this.commentForm.value.comment,
+    };
+    Object.assign(comment, { time: date });
+    //Star section
+    //In real application this should not divided by 2 this is only for mock up
+    this.productsDetail[0].star =
+      (this.productsDetail[0].star + this.rating) / 2;
+    // Comment section
+    this.productsDetail[0].comments.push(comment);
+    this.api
+      .updateItemApi(this.productsDetail[0], this.itemId)
+      .subscribe((res) => {
+        this.commentForm.reset();
+        this.toastr.success('Product Added', 'Success');
+      });
   }
 
-
-
-ngOnDestroy(): void {
-    this.sub$?.unsubscribe()
-}
-
+  ngOnDestroy(): void {
+    this.sub$?.unsubscribe();
+  }
 }
